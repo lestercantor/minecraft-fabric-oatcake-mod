797731fef37141f4ad5d255645998ec376552be0
LesterCantor <c012718j@student.staffs.ac.uk>
2024-06-17 02:05:26 +0100

    Initial commit

---
+++ src/main/java/net/magic497/oatcakemod/OatcakeModClient.java
@@ -0,0 +1,10 @@
+package net.magic497.oatcakemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OatcakeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
